1938173ad6bcee817d9385f9b432147cfa8c8384
원종빈의 MacBook <gghkdu2@gmail.com>
2021-07-02 15:24:53 +0900

    진행상황업데이트(유투브동영상)

--- js/main.js
+++ js/main.js
@@ -20,7 +20,7 @@ const badgeEl = document.querySelector('header .badges');
 
 // throttle - 동일 이벤트를 실행하는 경우 임의 반복주기
 window.addEventListener('scroll', _.throttle(function () {
-    console.log(window.scrollY);
+    // console.log(window.scrollY);
     if (window.scrollY > 500){
         // badgeEl.style.display = "none";
         // gsap.to(요소, 지속시간, 옵션);
@@ -57,7 +57,42 @@ new Swiper('.notice-line .swiper-container', {
     loop: true
 }); 
 
+new Swiper('.promotion .swiper-container', {
+    direction: 'horizontal',
+    slidesPerView: 3, // 한번에 보여줄 슬라이드 개수
+    spaceBetween: 10, // 슬라이드 사이 여백
+    centeredSlides: true, // 1번 슬라이드가 가운데 보이기
+    loop: true, 
+    // autoplay: {
+    //     delay: 5000
+    // },
+    pagination: {
+        el: '.promotion .swiper-pagination', // 페이지 번호 요소 선택자
+        clickable: true //사용자의 페이지 번호 요소 제어
+    },
+    navigation: {
+        prevEl:'.promotion .swiper-prev',
+        nextEl:'.promotion .swiper-next'
 
+    }
+
+}); 
+
+
+const promotionEl = document.querySelector('.promotion');
+const promotionToggleBtn = document.querySelector('.toggle-promotion');
+let isHidePromotion = false;
+
+promotionToggleBtn.addEventListener('click', function() {
+    isHidePromotion = !isHidePromotion // 반대가 되도록(!)
+    if(isHidePromotion) {
+        //숨김처리
+        promotionEl.classList.add('hide');
+    }else{
+        // 보임처리
+        promotionEl.classList.remove('hide');
+    }
+});
 
 
 
